07359f77492bd0e6f58eb0fe774937446b6148e3
Brooks Johnson <brooks.johnson@pm.me>
2024-01-18 00:26:15 -0800

    Setup component-library
    This depends on us having done a build in the component-library
    workspace.

--- packages/site/eleventy.config.js
+++ packages/site/eleventy.config.js
@@ -1,6 +1,9 @@
 const { execSync } = require("child_process");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy({
+    "../component-library": "js/component-library",
+  });
   eleventyConfig.on("eleventy.after", async () => {
     console.log("Building Tailwind…");
     console.log(execSync("npm run build:tailwind").toString());
