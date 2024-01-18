93aa8ed2572f22df9a44146d8461d9755713e1e4
Brooks Johnson <brooks.johnson@pm.me>
2024-01-17 23:42:50 -0800

    Setup plugin

--- packages/component-library/stencil.config.ts
+++ packages/component-library/stencil.config.ts
@@ -1,4 +1,18 @@
 import { Config } from '@stencil/core';
+import tailwind, { tailwindHMR, setPluginConfigurationDefaults } from 'stencil-tailwind-plugin';
+import tailwindcss from 'tailwindcss';
+import tailwindConf from '../../tailwind.config';
+import autoprefixer from 'autoprefixer';
+
+tailwindConf.content = [];
+
+setPluginConfigurationDefaults({
+  tailwindConf,
+  tailwindCssPath: './src/styles/tailwind.css',
+  postcss: {
+    plugins: [tailwindcss(), autoprefixer()],
+  },
+});
 
 export const config: Config = {
   namespace: 'component-library',
@@ -18,7 +32,8 @@ export const config: Config = {
       serviceWorker: null, // disable service workers
     },
   ],
+  plugins: [tailwind({}), tailwindHMR()],
   testing: {
-    browserHeadless: "new",
+    browserHeadless: 'new',
   },
 };
